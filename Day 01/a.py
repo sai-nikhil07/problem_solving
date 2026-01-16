3f3905247cebe423e6ca93a10b49b26608b05838
Sai Nikhil <sainikhilpatel07@gmail.com>
2026-01-16 14:59:26 +0530

    leet codenlongestCommonPrefix function
    Implement function to find the longest common prefix among a list of strings.

--- Day 01/a.py
+++ Day 01/a.py
@@ -1,1 +1,24 @@
+# https://leetcode.com/problems/longest-common-prefix/submissions/1886644384/?roomId=lwCIGB
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
 
+        shortest = min(strs, key=len)
+        l=len(shortest) #3
+        for i in range(l):
+            for s in strs:
+                if s[i] != shortest[i]:
+                    return shortest[:i]
+
+        return shortest
+
+
+# # -------- driver code (THIS PART IS IMPORTANT) --------
+# if __name__ == "__main__":
+#     sol = Solution()
+
+#     # Test cases
+#     print(sol.longestCommonPrefix(["flower", "flow", "flight"]))  # fl
+#     print(sol.longestCommonPrefix(["dog", "racecar", "car"]))     # ""
+#     print(sol.longestCommonPrefix(["interview", "internet", "internal"]))  # inte
