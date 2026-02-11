31f6e1d8631b92ae041b275daa8473d4c885a60d
Sai Nikhil <sainikhilpatel07@gmail.com>
2026-02-11 23:55:21 +0530

    leet code power of three

---
+++ Day 25/a.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/power-of-three/
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        
+        if n <=0:
+            return False
+
+        while n % 3==0:
+
+            n//=3
+        
+        return n==1
