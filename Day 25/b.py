cb9de44c876cb3f1eb9b1a4e81ced9dfb9f7a04c
Sai Nikhil <sainikhilpatel07@gmail.com>
2026-02-11 23:59:31 +0530

    leet code power of four

---
+++ Day 25/b.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/power-of-four/
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        
+        if n <= 0 :
+            return False
+        
+        while n%4==0:
+            
+            n//=4
+        
+        return n==1
