694619effecbae05738c499d81f05bad08c0e56a
Sai Nikhil <sainikhilpatel07@gmail.com>
2026-05-12 17:37:56 +0530

    Add Solution class with numberOfSteps method
    Implement numberOfSteps method to count steps to reduce a number to zero.

---
+++ Day 46/d.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        
+        c=0
+
+        while num:
+            if num %2==0:
+                num//=2
+            else:
+                num-=1
+            c+=1
+        return c
