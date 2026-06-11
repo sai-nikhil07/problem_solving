cd865887b5b41e2189d7d14830acf60d1d71bd05
Sai Nikhil <sainikhilpatel07@gmail.com>
2026-06-11 10:34:01 +0530

    Add findNumbers method to count even digit numbers
    Implement a method to count numbers with even digits.

---
+++ Day 50/c.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        
+        c= 0 
+        for i in nums:
+            if len(str(i))%2==0:
+                c+=1
+        
+        return c
