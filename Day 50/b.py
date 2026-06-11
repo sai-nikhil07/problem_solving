5d51d7dcb211e9aed52e6e8b35038e00479cc796
Sai Nikhil <sainikhilpatel07@gmail.com>
2026-06-11 10:27:03 +0530

    Add sumOfUnique method to Solution class
    Implement sumOfUnique method to calculate the sum of unique elements in a list.

---
+++ Day 50/b.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/sum-of-unique-elements/
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        
+        s=0
+
+        for i in nums:
+            if nums.count(i)==1:
+                s+=i
+        return s
